55e27fa9709caa0db8816e148132dc0e41862a2f
Long Hoang <inadev1997@gmail.com>
2024-12-19 22:22:46 +0700

    day18, 19

---
+++ d18.js
@@ -0,0 +1,52 @@
+const fs = require('node:fs');
+const file = fs.readFileSync(`input18.txt`, 'utf8');
+
+let input = file.trim().split("\n").map(b => b.split(",").map(c => parseInt(c)))
+
+let maxX = 70
+let maxY = 70
+
+function get(x, y, bytes) {
+    if (x < 0 || x > maxX || y < 0 || y > maxY) {
+        return "#"
+    }
+    for (let i = 0; i < bytes.length; i++) {
+        if (bytes[i][0] === x && bytes[i][1] === y) {
+            return "#"
+        }
+    }
+    return "."
+}
+
+function solvable(bytes) {
+    let moves = [{x: 0, y: 0, steps: 0}]
+    let seen = {}
+    while (moves.length > 0) {
+        let {x, y, steps} = moves.pop()
+        if (get(x, y, bytes) === "#") {
+            continue
+        }
+        if (x == maxX && y == maxY) {
+            return steps
+        }
+        let key = `${x},${y}`
+        if (seen[key]) {
+            continue
+        }
+        seen[key] = true
+        moves.push({x: x + 1, y, steps: steps + 1})
+        moves.push({x: x - 1, y, steps: steps + 1})
+        moves.push({x, y: y + 1, steps: steps + 1})
+        moves.push({x, y: y - 1, steps: steps + 1})
+        moves.sort((b, a) => a.steps - b.steps)
+    }
+    return -1
+}
+
+console.log("Part 1 " + solvable(input.slice(0, 1024)))
+for (let i = input.length - 1; i > 1024; i--) {
+    if (solvable(input.slice(0, i)) > 0) {
+        console.log(i + " at " + input[i])
+        break
+    }
+}
---
+++ d19.js
@@ -0,0 +1,36 @@
+const fs = require('node:fs');
+const file = fs.readFileSync(`input19.txt`, 'utf8');
+
+let towels = file.trim().split("\n\n")[0].split(", ")
+let designs = file.trim().split("\n\n")[1].split("\n")
+
+console.log()
+
+let cache = {}
+
+function possible(design) {
+    if (design.length == 0)
+        return 1
+    if (cache[design] !== undefined)
+        return cache[design]
+    let options = 0
+    for (let towel of towels) {
+        if (design.endsWith(towel)) {
+            options += possible(design.slice(0, design.length - towel.length))
+        }
+    }
+    cache[design] = options
+    return options
+}
+
+let part1 = 0
+let part2 = 0
+for (let design of designs) {
+    let options = possible(design);
+    if (options > 0)
+        part1++
+    part2 += options
+}
+
+console.log("Part 1 " + part1)
+console.log("Part 2 " + part2)
